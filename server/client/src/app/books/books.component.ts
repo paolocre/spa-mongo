7599046089f4c0f0fdac136932ba4d75a0572fbe
Paolo <paolo.cremaschi@lpgsystems.com>
2020-03-27 11:22:00 +0100

    init2

---
+++ server/client/src/app/books/books.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Book } from './book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent implements OnInit {
+
+  constructor(private apollo: Apollo) { }
+
+  displayedColumns: string[] = ['title', 'author'];
+  data: Book[] = [];
+  resp: any = {};
+  isLoadingResults = true;
+
+  ngOnInit(): void {
+    this.apollo.query({
+      query: gql `{ books { _id, title, author } }`
+    }).subscribe(res => {
+      this.resp = res;
+      this.data = this.resp.data.books;
+      console.log(this.data);
+      this.isLoadingResults = false;
+    });
+  }
+
+}
